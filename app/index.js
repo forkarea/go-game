8c2c62c18c589e16ffd00a8bd2104c1b88228cba
oprogramador <uomodislesia@gmail.com>
2017-09-08 16:20:18 +0200

    wgo

--- app/index.js
+++ app/index.js
@@ -1,11 +1,9 @@
 'use strict';
 import './style.scss';
+import './wgo/wgo';
 
-// randomly using ES7 object rest spread because it currently raises
-// an error in all browsers, but can be transpiled by Babel
-const { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 };
-const n = { x, y, ...z };
-if (Object.keys(n).map((key) => n[key]).reduce((p,v) => p + v) === 10) {
-  document.querySelector('#app').insertAdjacentHTML('afterbegin', '<h1>works.</h1>');
-}
-
+const board = new WGo.Board(document.getElementById("board"), {
+    width: 600,
+});
+board.addObject({ x:2,y:3,c:WGo.W })
+board.addObject({ x:1,y:3,c:WGo.B })
